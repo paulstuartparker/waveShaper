0c5b3d0f05aedf7cd3846d08cdeb1a4a81b7c852
paul <paulstuartparker@gmail.com>
2017-11-05 23:45:51 -0500

    working filter working comp

--- synth.js
+++ synth.js
@@ -1,39 +1,88 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const osc1 = audioCtx.createOscillator();
+const osc1Vol = audioCtx.createGain();
 const osc2 = audioCtx.createOscillator();
 const gainNode = audioCtx.createGain();
 const lfo = audioCtx.createOscillator();
+const lfoVol = audioCtx.createGain();
 const masterVol = audioCtx.createGain();
 const distortion = audioCtx.createWaveShaper();
 const distortionVol = audioCtx.createGain();
 const reverb = audioCtx.createConvolver();
 const reverbVol = audioCtx.createGain();
+const splitter = audioCtx.createChannelSplitter();
 const masterCompression = audioCtx.createDynamicsCompressor();
+const dry = audioCtx.createGain();
+const hpf = audioCtx.createBiquadFilter();
+const lpf = audioCtx.createBiquadFilter();
+hpf.type='highpass';
+lpf.type='lowpass';
+
 
 const volume = document.getElementById('volume');
 const frequency = document.getElementById('frequency');
 const lfoknob = document.getElementById('lfo');
 const distortionknob = document.getElementById('distortion');
 const distortionVolKnob = document.getElementById('distortionVol');
-
+const reverbVolKnob = document.getElementById('reverb-vol');
+const reverbButton = document.getElementById('reverb-button');
+const osc1Gain = document.getElementById('osc1-gain');
+const dryKnob = document.getElementById('dry');
+const lfoVolume = document.getElementById('lfo-volume');
+const thresholdKnob = document.getElementById('threshold');
+const kneeKnob = document.getElementById('knee');
+const ratioKnob = document.getElementById('ratio');
+const lpfFreq = document.getElementById('lpf-freq');
+const lpfQ = document.getElementById('lpf-Q');
 
 osc1.type = 'sine';
 osc1.frequency.value = 80;
 lfo.frequency.value = 0.0;
-lfo.connect(gainNode.gain);
-osc1.connect(gainNode);
 masterVol.gain.value = volume.value;
 osc1.start();
 lfo.start();
-gainNode.connect(distortion);
-gainNode.connect(masterVol);
+lfo.connect(gainNode.gain);
+osc1.connect(gainNode);
+gainNode.connect(lfoVol);
+lfoVol.connect(masterVol);
+osc1.connect(distortion);
+osc1.connect(osc1Vol);
+osc1Vol.connect(masterVol);
+
 distortion.connect(distortionVol);
 distortionVol.connect(masterVol);
-masterVol.connect(audioCtx.destination);
 
+
+
+
+//reverb section
+const convolver = audioCtx.createConvolver();
+const reverbBuffer = audioCtx.createBufferSource();
+const getSound = new XMLHttpRequest();
+getSound.open('GET', 'stalbans_a_mono.wav', true);
+getSound.responseType="arraybuffer";
+getSound.onload = function() {
+  audioCtx.decodeAudioData(getSound.response, function(buffer){
+    console.log(buffer);
+    convolver.buffer = buffer;
+  });
+  getSound.send();
+};
+
+
+//
+gainNode.connect(convolver);
+convolver.connect(reverbVol);
+reverbVol.connect(masterVol);
+masterVol.connect(lpf);
+lpf.connect(hpf);
+hpf.connect(masterCompression);
+// dry.connect(masterCompression);
+masterCompression.connect(audioCtx.destination);
+// masterVol.connect(convolver);
 
 function distortionCurve(amount = 0) {
-  const sampleRate = 44100;
+  const sampleRate = 22500;
   const curve = new Float32Array(sampleRate);
   const deg = Math.PI / 180;
 
@@ -50,10 +99,22 @@ function distortionCurve(amount = 0) {
 
   return curve;
 }
+
+lfoVolume.addEventListener('input', function(){
+  lfoVol.gain.value = lfoVolume.value;
+});
+
+dry.addEventListener('input', function() {
+  dry.gain.value = dryKnob.value;
+});
+
+osc1Gain.addEventListener('input', function() {
+  osc1Vol.gain.value = osc1Gain.value;
+});
+
 distortionknob.addEventListener('input', function() {
   distortion.curve = distortionCurve(distortionknob.value);
-  console.log(distortionknob.value);
-  console.log(distortion.curve);
+
 
   distortion.oversample = '4x';
 });
@@ -74,4 +135,40 @@ lfoknob.addEventListener('input', function() {
 
 distortionVolKnob.addEventListener('input', function() {
   distortionVol.gain.value = distortionVolKnob.value;
+});
+
+reverbVolKnob.addEventListener('input', function() {
+  reverbVol.gain.value = reverbVolKnob.value;
+
+});
+
+thresholdKnob.addEventListener('input', function() {
+  masterCompression.threshold.value = thresholdKnob.value;
+});
+
+kneeKnob.addEventListener('input', function() {
+  masterCompression.knee.value = kneeKnob.value;
+});
+
+ratioKnob.addEventListener('input', function() {
+  masterCompression.ratio.value = ratioKnob.value;
+});
+const minVal = 40;
+const maxVal = audioCtx.sampleRate / 2;
+lpfFreq.min = minVal;
+lpfFreq.max =maxVal;
+// function changeFrequency(element) {
+//   const numOctaves = Math.log(maxVal/minVal) / Math.LN2;
+//   let multiplier = Math.pow(2, numOctaves, element.value - 1.0);
+//   debugger
+//   return maxVal * multiplier;
+// }
+
+
+lpfFreq.addEventListener('input', function() {
+  lpf.frequency.value = lpfFreq.value;
+});
+
+lpfQ.addEventListener('input', function() {
+  lpf.Q.value = lpfQ.value;
 });
